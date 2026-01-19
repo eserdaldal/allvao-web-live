12e3affb8eca5bad23ea7963765a4d2b1371d795
eserdaldal <eser.daldal@gmail.com>
2026-01-19 21:02:48 +0400

    Guncelleme: Pzt 19.01.2026 21:02:48,67

--- muhurle.bat
+++ muhurle.bat
@@ -1,10 +1,10 @@
 @echo off
-echo 🛠️ Allvao Projesi Muhurleniyor...
+echo Allvao Projesi Muhurleniyor...
 git add .
 set /p msg="Degisiklik notu (bos birakirsan 'Guncelleme' yazilir): "
 if "%msg%"=="" set msg=Guncelleme: %date% %time%
 git commit -m "%msg%"
-echo 🚀 Kodlar GitHub'a firlatiliyor...
+echo Kodlar GitHub'a firlatiliyor...
 git push origin main
-echo ✅ Islem Tamamlandi! Allvao Guncellendi.
+echo Islem Tamamlandi! Allvao Guncellendi.
 pause
